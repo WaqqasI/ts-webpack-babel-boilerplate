ce7e804592e9580a2639fd30f2ea602ac87a3b82
Glitch (coconut-drink) <none>
2019-05-25 23:49:10 +0000

    🍟🚟 Checkpoint ./package.json:1396124/4125 ./webpack.config.js:1396124/866

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: "./dist"
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
