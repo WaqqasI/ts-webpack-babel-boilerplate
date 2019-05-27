18f7a7f07ff5faf0da082c0dbcd6a0d05f9ce1c7
Glitch (a-tarasyuk-react-webpack-typescript-babel) <none>
2019-05-27 15:09:34 +0000

    👙🚊 Checkpoint ./.glitch-assets:1396124/427 ./custom.d.ts:1396124/85 ./src/logo.svg:1396124/2670 ./src/index.tsx:1396124/53

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components/App';
 import _ from 'lodash/fp';
+import svgURL from './static/logo.svg'
 
-_.memoize((f) => f('hi'))(console.log)
+_.memoize((f) => f(svgURL))(console.log)
 
 ReactDOM.render(<App />, document.getElementById('root'));
