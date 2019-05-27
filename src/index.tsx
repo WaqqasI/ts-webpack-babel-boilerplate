f89446c642a33aabaced468b8a39cb12565d5e5b
Glitch (a-tarasyuk-react-webpack-typescript-babel) <none>
2019-05-27 14:58:24 +0000

    👨🍏 Checkpoint ./package.json:1396124/5640 ./src/index.tsx:1396124/145

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components/App';
+import _ from 'lodash/fp';
+
+_.memoize((f) => f('hi'))(console.log)
 
 ReactDOM.render(<App />, document.getElementById('root'));
